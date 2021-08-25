48c85cdf924bfbddebfcfd67722973dd5ba42b80
John He <hejohn2001@gmail.com>
2021-08-25 00:25:42 -0700

    started photos page, going to sleep for the day

---
+++ pages/photos.js
@@ -0,0 +1,14 @@
+import PhoneLayout from '../components/PhoneLayout.js';
+import styles from '../styles/Home.module.css';
+
+const Photos = () => {
+  return (
+    <PhoneLayout backgroundColor="#333438" content={(
+      <div>
+          
+      </div>
+    )} />
+  );
+}
+
+export default Photos;
