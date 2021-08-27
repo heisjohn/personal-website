080426ff6131037676f4c97162bac9bc94502cb9
John He <hejohn2001@gmail.com>
2021-08-26 23:19:29 -0700

    fix

--- components/AppIcon.js
+++ components/AppIcon.js
@@ -3,27 +3,30 @@ import Link from 'next/link';
 
 //props: destinationInSite (bool), destination (string), image, name
 export default function AppIcon(props) {
-  if (props.destinationInSite) {
-    return (
-      <div className={styles.appContainer}>
+  const getImage = () => {
+    if (props.destinationInSite) {
+      return (
         <Link href={props.destination} passHref>
           <div className={styles.appImageContainer} style={{backgroundImage: `url(${props.image || null})`}} />
         </Link>
-        <div className={styles.nameContainer}>
-          <h1 className={styles.appName}>{props.name}</h1>
-        </div>
-      </div>
-    )
-  } else {
+      )
+    }
     return (
-      <div className={styles.appContainer}>
-        <a href={props.destination} target="_blank" rel="noopener noreferrer">
-          <div className={styles.appImageContainer} style={{backgroundImage: `url(${props.image || null})`}} />
-        </a>
-        <div className={styles.nameContainer}>
-          <h1 className={styles.appName}>{props.name}</h1>
-        </div>
-      </div>
+      <a href={props.destination} target="_blank" rel="noopener noreferrer">
+        <div className={styles.appImageContainer} style={{backgroundImage: `url(${props.image || null})`}} />
+      </a>
     )
   }
+
+  return (
+    <div className={styles.appContainer}>
+      {getImage()}
+      {props.bottom ? 
+        <div className={styles.padding} /> : 
+        <div className={styles.nameContainer}>
+          <h1 className={styles.appName}>{props.name}</h1>
+        </div>
+      }
+    </div>
+  )
 }
