74208fcda954966aa0313f3d82644f3fd12bfd41
John He <hejohn2001@gmail.com>
2021-09-26 01:25:58 -0700

    change about me logo

--- pages/home.js
+++ pages/home.js
@@ -14,7 +14,7 @@ const Home = () => {
         <div className={styles.appContainer}>
           <div>
             <div className={styles.appRow}>
-              <AppIcon destination={"/about"} name="About Me" destinationInSite={true} image="/icons/me.png" />
+              <AppIcon destination={"/about"} name="About Me" destinationInSite={true} image="/icons/wikipedia.png" />
               <AppIcon destination={"/resume.pdf"} name="Resume" destinationInSite={false} image="/icons/resume.png" />
               <AppIcon destination={"https://www.linkedin.com/in/he-john/"} name="LinkedIn" destinationInSite={false} image="/icons/linkedin.png" />
               <AppIcon destination={"https://github.com/heisjohn/personal-website/"} name="Github" destinationInSite={false} image="/icons/github.png" />
@@ -29,7 +29,7 @@ const Home = () => {
         <div className={styles.bottomBar}>
           <div className={styles.appRow}>
             <AppIcon destination={"/resume.pdf"} name="" destinationInSite={false} image="/icons/resume.png" bottom />
-            <AppIcon destination={"/about"} name="" destinationInSite={true} image="/icons/me.png" bottom />
+            <AppIcon destination={"/about"} name="" destinationInSite={true} image="/icons/wikipedia.png" bottom />
             <AppIcon destination={"https://www.linkedin.com/in/he-john/"} name="" destinationInSite={false} image="/icons/linkedin.png" bottom />
           </div>
         </div>
